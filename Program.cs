77c07014208738bc5b2595a1ad07c4ca2f99e0dd
dimelouder <lolhh371@gmail.com>
2023-03-06 09:44:58 +0200

    2

--- Program.cs
+++ Program.cs
@@ -16,19 +16,94 @@ namespace ConsoleApp1
 
             //Console.ReadLine();
 
-            Random random = new Random();
-            int count = 0;
 
-            for (int i = 0; i < 15; i++)
-            {
-                double number = random.NextDouble() * 50 - 20;
-                Console.WriteLine(number);
-                if (number > 0) count++;
-            }
 
-            Console.WriteLine($"Количество положительных чисел: {count}");
+            //Random random = new Random();
+            //int count = 0;
 
-            Console.ReadLine();
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    double number = random.NextDouble() * 50 - 20;
+            //    Console.WriteLine(number);
+            //    if (number > 0) count++;
+            //}
+
+            //Console.WriteLine($"Количество положительных чисел: {count}");
+
+            //Console.ReadLine();
+
+
+
+            //Random rnd = new Random();
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    double num = rnd.NextDouble() * 10 - 5; // Генерируем число в интервале (-5,5)
+            //    if (num < 0)
+            //    {
+            //        Console.WriteLine("{0} - Отрицательное число", num);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("{0} - Положительное число", num);
+            //    }
+            //}
+
+
+
+            //Random random = new Random();
+
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int number = random.Next(-30, 31);
+            //    Console.Write(number + " ");
+
+            //    if (number < 0)
+            //    {
+            //        Console.Write("отрицательное ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("положительное ");
+            //    }
+
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.Write("четное\n");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("нечетное\n");
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //Random random = new Random();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int number = random.Next(-20, 21);
+            //    if (number > 0)
+            //    {
+            //        Console.Write(number + " ");
+
+            //        if (number % 2 == 0)
+            //        {
+            //            Console.Write("четное\n");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("нечетное\n");
+            //        }
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+
         }
     }
 }
+
